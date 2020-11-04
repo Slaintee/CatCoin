0f9266d8c317afe11d33b463f877732fdd7088d3
Ken Zhang <yzhang76@uvm.edu>
2020-11-04 11:17:17 +0800

    Semi-finished

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 Lab 7
 Ken Zhang
 CS 166 / Fall 2020
-
-
 """
 
-import sys
 import csv
+import sys
 
 
 def main_menu():
+    """
+    Menu function
+    :returns: the secondary menu, two functions and exit
+    """
     print('Please select from the following options:')
     print('    1. Check Wallet Balance')
     print('    2. Transfer CatCoins')
@@ -33,12 +35,16 @@ def main_menu():
 
 
 def check_balance():
+    """
+    Function to check balance from wallet.csv
+    :return: balance
+    """
     print('Which account would you like to check? (1/2/3)')
     account_number = input()
     while account_number != '1' and account_number != '2' and account_number != '3':
         print('Invalid input. Try again: ')
         account_number = input()
-    with open('balance.csv', 'r') as f:
+    with open('wallet.csv', 'r') as f:
         balance_line = f.readlines()
         for line in balance_line:
             bal = line.split(',')
@@ -64,8 +70,13 @@ def check_balance():
 
 
 def transfer():
+    """
+    Function to transfer coin from a wallet to another
+    :return: revised wallet.csv
+    """
+    # Open CSV and save amount in dictionary
     balance_dict = {}
-    with open('balance.csv', 'r') as f:
+    with open('wallet.csv', 'r') as f:
         balance_line = f.readlines()[1:]
         for line in balance_line:
             bal = line.split(',')
@@ -73,6 +84,7 @@ def transfer():
             balance = bal[1]
             balance = "".join(balance).replace('\n', '')
             balance_dict[wallet] = float(balance)
+    # Choose two wallets
     print('Choose a source wallet 1/2/3: ')
     source = input()
     while source != '1' and source != '2' and source != '3':
@@ -83,6 +95,7 @@ def transfer():
     while destination != '1' and destination != '2' and destination != '3' and destination == source:
         print('Invalid input. Try again: ')
         destination = input()
+    # Input amount
     try:
         print('Transfer amount: ')
         amount = float(input())
@@ -90,7 +103,7 @@ def transfer():
         print('Invalid input. Try again: ')
         amount = float(input())
     if amount <= 0:
-        print('Insufficient balance.')
+        print('Invalid amount.')
         print('-----------------------------------------')
         main_menu()
     else:
@@ -102,13 +115,14 @@ def transfer():
                 print('-----------------------------------------')
                 main_menu()
             else:
+                # Make changes and write to csv file
                 balance_dict[source] -= amount
                 balance_dict[destination] += amount
                 header = ['wallet number', 'balance']
                 data = [{'wallet number': '1', 'balance': balance_dict['1']},
                         {'wallet number': '2', 'balance': balance_dict['2']},
                         {'wallet number': '3', 'balance': balance_dict['3']}]
-                with open('balance.csv', 'w') as f:
+                with open('wallet.csv', 'w') as f:
                     writer = csv.DictWriter(f, fieldnames=header)
                     writer.writeheader()
                     writer.writerows(data)
